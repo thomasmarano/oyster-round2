90b4c8f83bc37c5471cc7b7d9d8223decb644a5b
thomasmarano <thomas.marano2@gmail.com>
2019-01-27 15:47:45 +0000

    Passing code for touch in/touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,33 @@
 class Oystercard
 
-    attr_reader :balance
+    attr_reader :balance, :in_journey, :travelling
+
+    MAX = 90
 
     def initialize
         @balance = 0
+        @travelling = false
     end
 
     def top_up(amount)
+        raise 'Exceeds Max Balance of #{MAX}' if (@balance + amount > MAX)
         @balance += amount
+    end
+
+    def deduct(amount)
+        @balance -= amount
+    end
+
+    def in_journey?
+        @travelling
+    end
+
+    def touch_in
+        @travelling = true
+    end
+
+    def touch_out
+        @travelling = false
     end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,9 +11,38 @@ describe Oystercard do
     end
 
     describe '#top_up' do
-        it 'increases the balance by 10' do
+        it 'can top up the balance' do
             expect{oystercard.top_up(10)}.to change{oystercard.balance}.by(10)
+        end
+
+        it 'throws an error if new amount exceeds max balance allowed' do
+            max_balance = Oystercard::MAX
+            expect{oystercard.top_up(max_balance + 1)}.to raise_error{'Exceeds Max Amount of #{max_balance}'}
         end
     end
 
+    describe '#deduct' do
+        it 'can deduct the balance' do
+            expect{oystercard.deduct(10)}.to change{oystercard.balance}.by(-10)
+        end
+    end
+
+    describe '#in_journey?' do
+        it 'is initially not in journey' do
+            expect(oystercard).not_to be_in_journey
+        end
+    end
+
+    describe '#touch_in' do
+        it 'can touch the oystercard in' do
+            expect{oystercard.touch_in}.to change{oystercard.travelling}.from(false).to(true)
+        end
+    end
+
+    describe '#touch_out' do
+        it 'can touch the oystercard out' do
+            oystercard.touch_in
+            expect{oystercard.touch_out}.to change{oystercard.travelling}.from(true).to(false)
+        end
+    end
 end
